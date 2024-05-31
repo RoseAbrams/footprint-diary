fda45cd7f1afb7ae5712973ba08779b1cd80410d
Rose Abrams <guagisorab@gmail.com>
2024-05-31 07:13:27 +0200

    New ideas since last night

---
+++ se/roseabrams/footprintdiary/entries/skype/SkypeMessage.java
@@ -0,0 +1,8 @@
+package se.roseabrams.footprintdiary.entries.skype;
+
+import se.roseabrams.footprintdiary.DiaryEntry;
+import se.roseabrams.footprintdiary.interfaces.Message;
+import se.roseabrams.footprintdiary.interfaces.PlainText;
+
+public class SkypeMessage extends DiaryEntry implements Message, PlainText {
+}
---
+++ se/roseabrams/footprintdiary/entries/wikimedia/WikimediaEdit.java
@@ -0,0 +1,16 @@
+package se.roseabrams.footprintdiary.entries.wikimedia;
+
+public class WikimediaEdit {
+    /*
+     * get data from combining these:
+     * https://en.wikisource.org/wiki/Special:Contributions/Rose_Abrams
+     * https://sv.wiktionary.org/wiki/Special:Contributions/Rose_Abrams
+     * https://meta.wikimedia.org/wiki/Special:Contributions/Rose_Abrams
+     * https://commons.wikimedia.org/wiki/Special:Contributions/Rose_Abrams
+     * https://en.wikiquote.org/wiki/Special:Contributions/Rose_Abrams
+     * https://en.wiktionary.org/wiki/Special:Contributions/Rose_Abrams
+     * https://sv.wikipedia.org/wiki/Special:Contributions/Rose_Abrams
+     * https://en.wikipedia.org/wiki/Special:Contributions/Rose_Abrams
+     * https://www.wikidata.org/wiki/Special:Contributions/Rose_Abrams
+     */
+}
