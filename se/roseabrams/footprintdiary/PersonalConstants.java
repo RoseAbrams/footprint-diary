d0a4d61d509f51c7b6d0a63dbeb17f50ea06d9da
Rosa Abrahamsson <rosa.abrahamsson@unicus.com>
2024-08-22 17:05:29 +0200

    Add some testing for date operations, seems to work

--- se/roseabrams/footprintdiary/PersonalConstants.java
+++ se/roseabrams/footprintdiary/PersonalConstants.java
@@ -6,7 +6,6 @@ public class PersonalConstants {
     public static final String DISCORD_USERNAME = "ormen3757";
     public static final String SKYPE_USERNAME = "Jennifer Olsson";
     public static final int SPOTIFY_USERNAME = 1144943067;
-    /// https://meta.wikimedia.org/wiki/Special:CentralAuth/Rose_Abrams
     public static final String[] WIKIS_WITH_EDITS = { "en.wikipedia", "en.wiktionary", "commons.wikimedia",
             "www.wikidata", "sv.wikipedia", "sv.wiktionary", "en.wikiquote", "en.wikisource", "meta.wikimedia" };
     public static final String FACEBOOK_NAME = "Rosa Luna Abr";
